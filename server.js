c6e914c741011b4f100df155c1752acef78025f0
nick sheng <hangjun.sheng@kaiyuandao.com>
2016-10-18 22:12:49 +0800

    服务端的模块放在哪里

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 var http = require("http");
 
-function onRequest(request, response) {
-  console.log('Request received');
-  response.writeHead(200, {
-    'Content-Type': 'text/plain'
-  });
-  response.write('hello world');
-  response.end();
+function start() {
+  function onRequest(request, response) {
+    console.log('Request received');
+    response.writeHead(200, {
+      'Content-Type': 'text/plain'
+    });
+    response.write('hello world');
+    response.end();
+  }
+
+  http.createServer(onRequest).listen(8888);
+  console.log('Server running at http://127.0.0.1:8888/');
 }
 
-http.createServer(onRequest).listen(8888);
-
-console.log('Server running at http://127.0.0.1:8888/');
+exports.start = start;
